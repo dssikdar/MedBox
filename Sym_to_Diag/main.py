3a88ac4504ac7853ddf4400beef85e1190ee74c2
jcwang111 <23427258+jcwang111@users.noreply.github.com>
2023-02-04 20:48:01 -0800

    post request success

--- Sym_to_Diag/main.py
+++ Sym_to_Diag/main.py
@@ -1,30 +1,34 @@
 from flask import Flask, render_template, request, redirect
 import database
 
-#from flask_cors import CORS
+from flask_cors import CORS
 
 #from main import #something
 
 app = Flask(__name__)
-#CORS(app)
+app.debug= True
+CORS(app)
 
 @app.route('/diagnoses', methods = ['POST', 'GET'])
 def get_diagnoses_drugs():
     if request.method == 'POST':
       symptoms = request.form.get('symptoms')
-      diagnose(symptoms)
-      return redirect("med.html", code=302)
+      #diagnose(symptoms)
+      return {'result':'apple'}
+    else:
+      return {'result':'getted'}
 
 @app.route("/withdraw", methods = ['POST', 'GET'])
 def decrement_inventory():
-   if request.method == 'POST':
+    if request.method == 'POST':
       name = request.form.get('name')
       a_drug = get_drug("Salt")
       if a_drug['inventory'] > 0:
         database.update_stock(a_drug['name'], a_drug['inventory']-1)
         
         return True
-
+    else:
+      return {'result':'getted'}
 @app.route("/dothis", )
 def get_drug():
     return get_drug(request.args.get('drug_name'))
