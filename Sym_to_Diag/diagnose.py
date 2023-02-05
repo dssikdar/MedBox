750ee5f2b7e03a5a12522726330db4cc82d28e9f
Diptanshu Sikdar <dssikdar@gmail.com>
2023-02-04 16:42:01 -0800

    added work for symptoms to diagnosis

---
+++ Sym_to_Diag/diagnose.py
@@ -0,0 +1,66 @@
+#
+#
+# main() will be run when you invoke this action
+#
+# @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+#
+# @return The output of this action, which must be a JSON object.
+#
+#
+from ApiMedicClass import DiagnosisClient
+
+def return_sym_ids(user_symptoms, all_symptoms) -> list:
+    sym_ids = []
+    for symptom in user_symptoms:
+        for i in all_symptoms:
+            if i["Name"].lower() == symptom.lower():
+                sym_ids.append(i["ID"])
+    return sym_ids
+
+def filter_keywords(output: dict) -> list:
+    treatment_description = output.get('TreatmentDescription')
+    list_of_words = treatment_description.split()
+    return [word for word in list_of_words if word[0].isupper()]
+
+def get_most_probable_diagnosis(output: dict) -> str:
+    return output.get('Name')
+
+def run(user_symptoms: list, gender: str, yob: str) -> dict:       
+    d = DiagnosisClient(username="s2NEp_GMAIL_COM_AUT",language="en-gb",healthServiceUrl="https://healthservice.priaid.ch",authServiceUrl="https://authservice.priaid.ch/login",password="f9H6Brm5Z3NyFk8p7")
+      
+    all_symptoms = d.loadSymptoms()
+
+    relevant_sym_ids = return_sym_ids(user_symptoms, all_symptoms)
+                
+    do = d.loadDiagnosis(gender=gender, yearOfBirth=int(yob), selectedSymptoms=relevant_sym_ids)
+
+    issues = {}
+    if do:
+        issues = {"Message":""}
+        for i in range(len(do)):
+            if len(do)-1 == i:
+                issues["Message"] = issues["Message"]+ do[i]["Issue"]["Name"] + " (Accuracy: " + str(do[i]["Issue"]["Accuracy"])[:4] +"% ) "
+            elif len(do)-2==  i:
+                issues["Message"] = issues["Message"]+ do[i]["Issue"]["Name"] + " (Accuracy: " + str(do[i]["Issue"]["Accuracy"])[:4] +"% ) "+" and "
+            else:
+                issues["Message"] = issues["Message"]+ do[i]["Issue"]["Name"] + " (Accuracy: " + str(do[i]["Issue"]["Accuracy"])[:4] +"% ) "+" , "
+    else:
+        issues = {"Message":"I could not diagnose by entered symptoms."} 
+        
+    return issues
+
+
+    
+if __name__ == '__main__':
+    # option: 1 or 2
+    # issue: choose one among csv
+
+    symptoms = ['cough', 'fever', 'headache']
+    gender = 'Male'
+    yob = 1999
+
+    diagnosis = run(symptoms, gender, yob)
+
+    #print(filter_keywords(diagnosis))
+    #print(get_name(diagnosis))
+    print(diagnosis)
