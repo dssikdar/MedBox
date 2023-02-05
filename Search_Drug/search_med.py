e6480bd51d46900b503cc80e4b7e363db7e99988
Diptanshu Sikdar <dssikdar@gmail.com>
2023-02-05 01:57:25 -0800

    search drugs updated

--- Search_Drug/search_med.py
+++ Search_Drug/search_med.py
@@ -1,10 +1,17 @@
 from pathlib import Path
+from meds import all_medications
 
 def search_medication(drug_query: str, file_path: Path) -> str:
     with open(file_path, 'r', encoding='utf-8') as drugs_file:
         for line in drugs_file:
             if drug_query in line:
                 return line
+        return 'No matches found'
+            
+def search_med_via_diag(diag_query: str) -> list:
+    for diag in all_medications.keys():
+        if diag_query in diag:
+            return all_medications[diag]
             
 if __name__ == '__main__':
     my_query = 'Tylenol'
